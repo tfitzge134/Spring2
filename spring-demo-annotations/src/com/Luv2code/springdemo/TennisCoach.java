9eae7cf9d792c43ba5e95fe7ec112c0a75dace10
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-11-28 08:55:21 -0600

    added qualifiers

--- spring-demo-annotations/src/com/Luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/Luv2code/springdemo/TennisCoach.java
@@ -1,11 +1,15 @@
 package com.Luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	//setup private fiels x fortuneService to be injected
+	//AUTOWIRED APPLY INJECTION TO THE FIELD
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	//define a default constructor to show what spring is doing behind scene
 	public TennisCoach() {
@@ -13,12 +17,12 @@ public class TennisCoach implements Coach {
 	}
 //define a setter method
 	//autowired find any depedencies for the fortuneserive
-	@Autowired
-	public void setFortuneService(FortuneService theFortuneService) {
-		System.out.println("tenniscoach:>>>> inside the setFortuneService method");
-		fortuneService=theFortuneService;
-		
-	}
+//	@Autowired
+//	public void doSomeCrazystuff(FortuneService theFortuneService) {
+//		System.out.println("tenniscoach:>>>> inside the doSOMECRAZYSTUFF");
+//		fortuneService=theFortuneService;
+//		
+//	}
 //	@Autowired
 ////create a constructor to pass fortuneservice
 //	public TennisCoach(FortuneService theFortuneService) {
